0613e26d53bbe7bce4f64ef132535d42b5323e28
avelasquezz <juanandresv2004@gmail.com>
2024-11-29 00:20:52 -0500

    Add default user

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,6 @@ import repository.*;
 import service.*;
 
 import view.LoginView;
-
 import model.User;
 
 public class Main {
@@ -14,24 +13,21 @@ public class Main {
         SupplierRepository supplierRepository = new SupplierRepository();
         MovementRepository movementRepository = new MovementRepository();
         InventoryRepository inventoryRepository = new InventoryRepository();
+        NotificationRepository notificationRepository = new NotificationRepository();
+        OrderRepository orderRepository = new OrderRepository();
         
         UserService userService = new UserService(userRepository);
         ProductService productService = new ProductService(productRepository);
         SupplierService supplierService = new SupplierService(supplierRepository);
         MovementService movementService = new MovementService(movementRepository);
         InventoryService inventoryService = new InventoryService(inventoryRepository);
+        NotificationService notificationService = new NotificationService(notificationRepository);
+        OrderService orderService = new OrderService(orderRepository);
               
-        LoginView loginView = new LoginView(userService, productService, supplierService, movementService, inventoryService);
+        LoginView loginView = new LoginView(userService, productService, supplierService, movementService, inventoryService, notificationService, orderService);
 
-        User exampleUser1 = new User("ID", 1111, "John", "Doe", "12345", "john@example.com", "password123", true);
-    	User exampleUser2 = new User("ID", 2222, "Jane", "Smith", "54321", "jane@example.com", "mypassword", true);
-    	User exampleUser3 = new User("Passport", 3333, "Alice", "Johnson", "43983", "alice@example.com", "securepass", true);
-    	User exampleUser4 = new User("ID", 4444, "Bob", "Brown", "67890", "bob@example.com", "123", true);
-
-        userRepository.addUser(exampleUser1);
-        userRepository.addUser(exampleUser2);
-        userRepository.addUser(exampleUser3);
-        userRepository.addUser(exampleUser4);
+        User defaultUser = new User("ID", 1001, "Default", "User", "12345", "user@user.com", "123", true, "Administrador");
+        userRepository.addUser(defaultUser);
 
         loginView.showWindow();
     }
